b74b0d6858311bee591ab0cae79bc880e95ccfe5
lenovo <ilhamhadi0111@gmail.com>
2024-09-18 04:57:00 -1200

    fixing

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "birthday-app-v1";
+const urlsToCache = ["/", "/style/main.css", "/script/main.js", "/music/hbd.mpeg", "/img/irene.jpg", "/img/ballon1.svg", "/img/ballon2.svg", "/img/ballon3.svg", "/img/hat.svg", "/img/favicon.png"];
+
+// Install Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate the Service Worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch the resources
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
